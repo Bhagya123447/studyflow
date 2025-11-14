f792d0c701a6089fde3f82d42c8067e2159b1c06
Aksh2758 <akshu290110@gmail.com>
2025-11-15 00:16:48 +0530

    update feature

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMZgJ0UKlQTO2Mn5WtRTZQHOdN5_rxm7s",
-  authDomain: "studyflow-tracker.firebaseapp.com",
-  projectId: "studyflow-tracker",
-  storageBucket: "studyflow-tracker.firebasestorage.app",
-  messagingSenderId: "632480408917",
-  measurementId: "G-WK2J9DKH10",
-  appId: "1:632480408917:web:4f68c188ebaee7bcd4bbdc"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "sYOUR_AUTH_DOMAIN",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  measurementId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
